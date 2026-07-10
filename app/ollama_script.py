47782f46fe9d436a8bb86de390b64078b74dc19f
Arrnnnvva <arnav10426@gmail.com>
2026-07-10 06:21:40 +0530

    JSON output from the ollama_script

--- app/ollama_script.py
+++ app/ollama_script.py
@@ -7,12 +7,14 @@ def chat(prompt: str, model: str = "qwen3:4b-instruct", stream: bool = False) ->
     text = {
         "model": model,
         "messages" :[
+            {"role": "system", "content": "You must respond only with valid JSON."},
             {"role": "user", "content": prompt}
         ],
         "stream": stream,
         "options": {
             "temperature": 0.0
-        }
+        },
+        "format": "json"
     }
 
     response = requests.post(url=url, json=text)
@@ -26,7 +28,7 @@ def chat(prompt: str, model: str = "qwen3:4b-instruct", stream: bool = False) ->
     return data["message"]["content"]
 
 if __name__ == "__main__":
-    prompt = "HI can u tell me about LLMs"
+    prompt = "HI"
     Nonstream = chat(prompt=prompt, stream = False)
 
     print(Nonstream)
